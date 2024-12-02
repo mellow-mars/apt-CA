5afb5fe888f5df502a3b748271f9ed869e38c1e0
BeomJung Kim <kbeom0124@naver.com>
2024-12-02 10:11:11 +0900

    전화번호 중복 및 검증 추가

--- apt/views.py
+++ apt/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import CustomerForm
+from django.db.models import Q
+from .models import Customer
+import re
 
 def index(request):
     return render(request, 'index.html')
@@ -37,9 +40,19 @@ def apply(request):
     if request.method == 'POST':
         form = CustomerForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, '등록되었습니다.')
-            success = True
+            phone_number = form.cleaned_data.get('phone_number')
+
+            # 전화번호 유효성 검사 (11자리 또는 13자리 숫자와 '-'만 허용)
+            if not re.fullmatch(r'^\d{11}$|^\d{3}-\d{4}-\d{4}$', phone_number):
+                form.add_error('phone_number', '전화번호는 11자리 또는 13자리 숫자와 "-"만 사용 가능합니다.')
+            else:
+                # 중복된 전화번호 확인
+                if Customer.objects.filter(Q(phone_number=phone_number)).exists():
+                    messages.error(request, '이미 등록된 전화번호입니다.')
+                else:
+                    form.save()
+                    messages.success(request, '등록되었습니다.')
+                    success = True
     else:
         form = CustomerForm()
 
