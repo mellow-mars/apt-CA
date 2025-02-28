3650abad9a6f8d2a1be75425c1351ce0700be3ee
xogns3725 <xogns3725@naver.com>
2025-03-01 00:24:58 +0900

    청주

--- apt_ca/urls.py
+++ apt_ca/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
     path('cheonan/', include('cheonan.urls'),),
     path('cheongju/', include('cheongju.urls')),
     path('yangju/', include('yangju.urls')),
